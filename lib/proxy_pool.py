69ad7fadb454b7bbc19efe26fed0ccf9f0dd1da2
Loy <puregithub@yeah.net>
2016-08-24 20:32:18 +0800

    Add proxy_port_diff option.

--- lib/proxy_pool.py
+++ lib/proxy_pool.py
@@ -7,9 +7,10 @@ class ProxyPool:
     @classmethod
     def get_ports(cls):
         base = options.proxy_port_start
+        diff = options.proxy_port_diff
         try:
             cls.i = (cls.i + 1) % options.proxy_port_pairs   
         except AttributeError:  # cls.i has not been defined yet
             cls.i = 0
-        return (base + cls.i * 2, 
-                base + cls.i * 2 + 1)
+        return (base + cls.i, 
+                base + cls.i + diff)
