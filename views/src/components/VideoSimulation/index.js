8bdee2cf540ffd6371c7eae873d9986b449c0527
Loy <puregithub@yeah.net>
2016-09-08 11:14:09 +0800

    Fix bug in streaming.

--- views/src/components/VideoSimulation/index.js
+++ views/src/components/VideoSimulation/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import scriptLoader from 'react-async-script-loader'
+import scriptLoader from 'react-async-script-loader';
+import { connect } from 'react-redux';
 
 import style from './style';
 import store from '../../redux/store';
@@ -11,7 +12,7 @@ export const init = () => {
     live_host, rtmp_port, rtmp_app, stream_key, hls_path
   } = store.getState().device;
 
-  if (!(rtmp_host && rtmp_port && rtmp_app && stream_key && hls_path)) {
+  if (!(live_host && rtmp_port && rtmp_app && stream_key && hls_path)) {
     store.dispatch(displayError('获取视频信息错误，请刷新重试'));
     return
   }
@@ -32,6 +33,13 @@ export const init = () => {
 @scriptLoader(
   '/static/vendor/jwplayer.js'
 )
+@connect(
+  (state) => ({
+    live_host: state.device.live_host,
+    hls_path: state.device.hls_path,
+    stream_key: state.device.stream_key
+  })
+)
 class VideoSimulation extends React.Component {
 
   componentWillUnmount() {
@@ -39,12 +47,11 @@ class VideoSimulation extends React.Component {
   }
 
   render() {
+    const { live_host, hls_path, stream_key } = this.props;
     return (
       <div className={style.media} id="mediaspace">
-        {/*
-        <video src={`http://${location.hostname}/hls/live.m3u8`}
+        <video src={`http://${live_host}/${hls_path}/${stream_key}.m3u8`}
                width="550" height="450" autoPlay controls></video>
-        */}
       </div>
     );
   }
